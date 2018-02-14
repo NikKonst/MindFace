7b575c98aee7875d481249c1d24efaf3983f525d
NikKonst <nikitakonst@mail.ru>
2018-02-14 14:16:38 +0300

    Initial Commit

---
+++ MindFace/MindFace.h
@@ -0,0 +1,19 @@
+//
+//  MindFace.h
+//  MindFace
+//
+//  Created by Nikita Konstantinovskiy on 14.02.2018.
+//  Copyright © 2018 nikkonst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MindFace.
+FOUNDATION_EXPORT double MindFaceVersionNumber;
+
+//! Project version string for MindFace.
+FOUNDATION_EXPORT const unsigned char MindFaceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MindFace/PublicHeader.h>
+
+
---
+++ MindFaceTests/MindFaceTests.swift
@@ -0,0 +1,36 @@
+//
+//  MindFaceTests.swift
+//  MindFaceTests
+//
+//  Created by Nikita Konstantinovskiy on 14.02.2018.
+//  Copyright © 2018 nikkonst. All rights reserved.
+//
+
+import XCTest
+@testable import MindFace
+
+class MindFaceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
